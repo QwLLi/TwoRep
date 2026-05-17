06b19bc2bc52f872535c2af30831f8da78c4d2e1
Vladislav Mokhov <mohov-vlad1998@mail.ru>
2026-05-17 10:28:04 +0300

    hibernate правки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Влад", "Влад", (byte) 5);
+        userService.saveUser("Коля", "Коля", (byte) 6);
+        userService.saveUser("Илья", "Илья", (byte) 7);
+        userService.saveUser("Даня", "Даня", (byte) 8);
+
+        //   System.out.println(userService.getAllUsers());
+
+        List<User> users = userService.getAllUsers();
+        for( User user : users){
+            System.out.println(user);
+        }
+
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
